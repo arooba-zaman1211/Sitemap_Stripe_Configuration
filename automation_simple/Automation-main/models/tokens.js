171105f03271658a15c19ab35c77813e48760981
Geeky <124719983+arooba-zaman1211@users.noreply.github.com>
2025-03-08 17:28:55 +0500

    Delete automation_simple/Automation-main directory

--- automation_simple/Automation-main/models/tokens.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const AccessTokenSchema = new mongoose.Schema({
-  token: { type: String, required: true },
-  expirationDate: { type: Date, required: true },
-});
-
-module.exports = mongoose.model("tokens", AccessTokenSchema);
